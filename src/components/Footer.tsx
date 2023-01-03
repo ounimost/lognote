7ebc5027dcd85af8463fcae6ce8c923ef1d49dad
ouni <>
2023-01-03 16:26:19 +0900

    Footerのリンクを修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 p-3 sticky inset-x-0 bottom-0">
       <p className="text-white text-xs text-center">
-        <a href="https://ouni.work">&copy;2022 OUNI</a>
+        <a href="https://ouni.jp">&copy;2022 OUNI</a>
       </p>
     </footer>
   );
